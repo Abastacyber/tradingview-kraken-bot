1ac07eafcc33c8d1430ae6cbe21cd32184c78f04
Abastacyber <ad.ferard@gmail.com>
2025-08-25 09:45:21 +0200

    gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,16 +1,15 @@
 # gunicorn.conf.py
-# =================
 # Logs concis + utiles pour Render
 
-# On garde les access/error logs sur stdout/stderr (Render les capte)
+# Laisse access/error logs sur stdout/stderr (Render les capte)
 accesslog = "-"
-errorlog  = "-"
-loglevel  = "info"
+errorlog = "-"
+loglevel = "info"
 
-# Format court : [date] statut PATH durée_ms UA
-#   %(t)s  -> date
-#   %(s)s  -> HTTP status
-#   %(U)s  -> chemin (sans query)
-#   %(M)s  -> durée en ms
-#   %(a)s  -> user-agent
-access_log_format = "%(t)s | %(s)s | %(U)s | %(M)sms | UA:%(a)s”
+# Format court lisible : [date] statut PATH durée_ms UA
+#  %(t)s -> date
+#  %(s)s -> HTTP status
+#  %(U)s -> chemin (sans query)
+#  %(M)s -> durée en ms
+#  %(a)s -> user-agent
+access_log_format = '%(t)s | %(s)s | %(U)s | %(M)sms | UA:%(a)s’
