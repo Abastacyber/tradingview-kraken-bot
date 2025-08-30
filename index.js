e9d57d3d7b179f7fe7ab89cb2c627a7e08b8c1e2
Abastacyber <ad.ferard@gmail.com>
2025-08-30 23:10:04 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+{
+"name": "tradingview-alert-server",
+"version": "1.0.0",
+"description": "Un simple serveur Node.js pour recevoir des alertes de TradingView.",
+"main": "index.js",
+"scripts": {
+"start": "node index.js"
+},
+"dependencies": {
+"body-parser": "^1.20.2",
+"express": "^4.19.2"
+}
+}
