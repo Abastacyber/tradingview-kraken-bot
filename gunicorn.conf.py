3b5666f475d00df24f5170438134d3e9c84c8f44
Abastacyber <ad.ferard@gmail.com>
2025-11-01 17:27:00 +0100

    gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,8 @@
 accesslog = "-"
 errorlog  = "-"
-loglevel  = "debug"
+loglevel  = "info"        # ou "debug" si tu veux plus de verbosité
 worker_class = "gthread"
 threads = 1
 timeout = 120
+graceful_timeout = 30
+keepalive = 5
