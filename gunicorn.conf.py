592814491bc11b9e407c69ac7513c1569ce32c55
Abastacyber <ad.ferard@gmail.com>
2025-11-01 11:58:32 +0100

    gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,3 +1,8 @@
 accesslog = "-"
 errorlog  = "-"
-loglevel  = "info"
+loglevel  = "debug"
+
+# >>> important
+worker_class = "gthread"   # active les threads
+threads      = 2           # 2 threads par worker
+timeout      = 120         # 120s avant kill d'un worker bloqué
